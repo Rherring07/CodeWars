bb002b1cb07928adf11976db04b77f34670cd1fa
Rherring07 <110481644+Rherring07@users.noreply.github.com>
2022-08-22 13:12:37 -0400

    Create duplicateEncounter.js

---
+++ Level 6 Kyu/duplicateEncounter.js
@@ -0,0 +1,40 @@
+//The goal of this exercise is to convert a string to a new string where each character in the new string is "(" 
+//if that character appears only once in the original string, or ")" if that character appears more than once in the original string. 
+//Ignore capitalization when determining if a character is a duplicate.
+
+//Examples
+//"din"      =>  "((("
+//"recede"   =>  "()()()"
+//"Success"  =>  ")())())"
+//"(( @"     =>  "))((" 
+
+//using for loop 
+function duplicateEncode(word){
+    // ...
+  //function ignores capitalization
+  word = word.toLowerCase();
+  //new string
+  let str = "";
+  //for each letter in the word, if word includes letter, add  ")"
+ for(let i = 0; i < word.length; i++) {
+   if(word.split(word[i]).length-1 > 1) 
+     str += ")";
+   else 
+     str += "("
+ }
+  
+  return str;
+  //else add "("
+}
+
+
+//using map
+function duplicateEncode(word){
+  return word
+    .toLowerCase()
+    .split('')
+    .map( function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+    })
+    .join('');
+}
